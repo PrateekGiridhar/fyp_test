a0303e1897df3f7dc12a13d7a178478df8f7be51
PrateekGiridhar <prateek.giridhar2000@gmail.com>
2022-05-13 19:52:29 +0530

    Starting the web3 testing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,49 @@
+import { useEffect, useState } from 'react';
+import './App.css'
+import VoterLogin from './VoterLogin'
+import Web3 from 'web3';
+
 
 const App = () => {
+
+
+  const [admin,setAdmin] = useState("")
+  //const [loader,setLoader] = useState(true)
+  //const [election,setElection] = useState()
+
+  useEffect(() => {
+    loadweb3();
+    LoadBlockchainData();
+});
+
+  const loadweb3 = async () => {
+    if(window.ethereum){
+      window.web3 = new Web3(window.ethereum);
+      await window.eth_requestAccounts;
+    }
+    else if(window.web3){
+      window.web3 = new Web3(window.web3.currentProvider)
+    }
+    else {
+      window.alert(
+        "Install metamask in order to proceed."
+      )
+    }
+  }
+
+  const LoadBlockchainData = async () => {
+    const web3 = window.web3
+    const accounts = await web3.eth.getAccounts()
+    console.log(accounts)
+    setAdmin(accounts[0])
+    console.log(admin)
+
+  }
+
   return (
-    <div className='container'>
-      <h1>Hello From App</h1>
+    <div className='App'>
+      <VoterLogin />
+      
     </div>
   )
 
